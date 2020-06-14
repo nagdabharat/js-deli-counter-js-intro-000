bd4e781e90acf583f06cac2ed1290b9c2b461771
Bharat Nagda <nagdabharat@gmail.com>
2020-06-14 06:29:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function takeANumber(line, name) {
   line.push(name);
   return(`Welcome, ${name}. You are number ${line.length} in line.`)
 }
+
+function currentLine(line) {
+  if(line.length == 0) {
+    return('The line is currrently empty.');
+  } else {
+    var temparray = []
+    for (i = 0; i < line.length; i++) {
+      
+    }
+  }
+  
+}
