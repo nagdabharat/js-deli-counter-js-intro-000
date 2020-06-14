53fc1fbe2086c128e22b404d8c56cd693f20d080
Bharat Nagda <nagdabharat@gmail.com>
2020-06-14 07:25:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function nowServing(line) {
   if(line.length == 0)  {
     return("There is nobody waiting to be served!");
   } else {
-
+      return(`Currently serving ${line.shift()}.`)
+      
   }
 }
